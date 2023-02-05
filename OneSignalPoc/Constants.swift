f735c774908801926be2713475008e8f216badd1
Harry-KNIGHT <eliott.knight@icloud.com>
2023-02-05 09:52:42 +0100

    Add gitignore and constant for keys.

---
+++ OneSignalPoc/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  OneSignalPoc
+//
+//  Created by Elliot Knight on 05/02/2023.
+//
+
+import Foundation
+
+
+let oneSignalAppId = "86b10622-4102-42b2-ab95-a0f82167cf3b"
+let oneSignalApiKey = "NjRjODU3MGMtMDgzZi00NmZhLTk4ZmMtMTY2MjU5YWFmZmNi"
